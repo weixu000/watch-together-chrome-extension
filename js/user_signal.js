b50fe2cfd0bdf8a6fe353c54908d83fb7d463082
Xu Wei <18422432+weixu000@users.noreply.github.com>
2022-08-01 22:29:36 +0800

    signal by user copy/paste

---
+++ js/user_signal.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ *
+ * @param {HTMLDivElement} div
+ */
+function UserSignalHelper(div) {
+  this.sendSignal = function (name, s) {
+    div.querySelector("p").textContent = JSON.stringify(s);
+  };
+
+  const signal = new Promise((r) => {
+    div.querySelector("button").addEventListener("click", () => {
+      r(JSON.parse(div.querySelector("input[type=text]").value));
+    });
+  });
+
+  this.recvSignal = async function (name) {
+    return await signal;
+  };
+}
